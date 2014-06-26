744b72dafe3f33c3ec2e6a18039c30ee45ae8638
hebertdougl <hebertdougl@gmail.com>
2014-06-25 21:20:57 -0300

    Refatorando o cliente.c

--- MQL4/Files/cliente.c
+++ MQL4/Files/cliente.c
@@ -2,18 +2,19 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#define TAMANHO_STRING 50
 
 int main (void){
     void *contexto = zmq_ctx_new ();
     void *requisicao = zmq_socket (context, ZMQ_REQ);
-    char conteudoServidor[10];
+    char conteudoServidor[TAMANHO_STRING];
 
     zmq_connect (requisicao, "tcp://localhost:5555");
     printf ("Conectando com o servidor...\n");
 
     for (;;) {
-        zmq_send (requisicao, "Ola", 5, 0);
-        zmq_recv (requisicao, contexto, 10, 0);
+        zmq_send (requisicao, "robo1", TAMANHO_STRING, 0);
+        zmq_recv (requisicao, contexto, TAMANHO_STRING, 0);
     }
     
     zmq_close (requisicao);
