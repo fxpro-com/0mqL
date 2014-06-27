fbe35641a360973cf8d54290b7f2804bf6a3d472
hebertdougl <hebertdougl@gmail.com>
2014-06-27 18:07:20 -0300

    Refatorando o servidor

--- servidor/MQL4/Files/servidor.c
+++ servidor/MQL4/Files/servidor.c
@@ -5,18 +5,15 @@
 #include <assert.h>
 #include <string.h>
 
-#define TAMANHO_STRING 50
+#define TAMANHO_STRING 10
 
 int main (void){
     FILE *arquivoCorrelacao, *arquivoFibonacci, *arquivoMediaMovel;
-    char correlacao[TAMANHO_STRING], fibonacci[TAMANHO_STRING], 
-         mediaMovel[TAMANHO_STRING], conteudoCliente [TAMANHO_STRING];
-    void *contexto, *resposta
-    int conexao;
-
-    *contexto = zmq_ctx_new ();
-    *resposta = zmq_socket (contexto, ZMQ_REP);
-    conexao = zmq_bind (resposta, "tcp://*:5555");
+    char correlacao[TAMANHO_STRING], fibonacci[TAMANHO_STRING], mediaMovel[TAMANHO_STRING];
+    char conteudoCliente[10];
+    void *contexto = zmq_ctx_new ();
+    void *resposta = zmq_socket (contexto, ZMQ_REP);
+    int conexao = zmq_bind (resposta, "tcp://*:5555");
 
     assert (conexao == 0);
 
@@ -24,40 +21,34 @@ int main (void){
     	arquivoCorrelacao = fopen("correlacao.txt","rt");
     	fscanf(arquivoCorrelacao, "%s",correlacao);
 
-        arquivoFibonacci = fopen("fibonacci.txt","rt");
+	arquivoFibonacci = fopen("fibonacci.txt","rt");
     	fscanf(arquivoFibonacci, "%s",fibonacci);
 
-        arquivoMediaMovel = fopen("mediaMovel.txt","rt");
+	arquivoMediaMovel = fopen("mediaMovel.txt","rt");
     	fscanf(arquivoMediaMovel, "%s",mediaMovel);
 
-        zmq_recv (resposta, conteudoCliente, TAMANHO_STRING, 0);
+        zmq_recv (resposta, conteudoCliente, 10, 0);
 
-    	if(strcmp(conteudoCliente, "robo1") == 0){
-        	strcat(correlacao, " ");
-        	strcat(correlacao, fibonacci);
-        	strcat(correlacao, " ");
-        	strcat(correlacao, mediaMovel);
+    	if(conteudoCliente[8] == 'o' || conteudoCliente[8] == '0'){
     		zmq_send (resposta, correlacao,TAMANHO_STRING, 0);
-    		printf("Requisicao do robo 1: %s\n",conteudoCliente);
+    		printf("%s",conteudoCliente);
     	}
-    	if(strcmp(conteudoCliente, "robo2") == 0){
-        	strcat(correlacao, " ");
-        	strcat(correlacao, fibonacci);
-    		zmq_send (resposta, correlacao , TAMANHO_STRING, 0);
-    		printf("requisicao do robo: %s\n",conteudoCliente);
+    	if(conteudoCliente[8] == 'i' || conteudoCliente[8] == 'I'){
+    		zmq_send (resposta, fibonacci ,TAMANHO_STRING , 0);
+    		printf("%s",conteudoCliente);
     	}
-        if(strcmp(conteudoCliente, "robo3") == 0){
-    		zmq_send (resposta, correlacao , TAMANHO_STRING, 0);
-    		printf("requisicao do robo: %s\n",conteudoCliente);
+	if(conteudoCliente[9] == 'l' || conteudoCliente[8] == 'L'){
+    		zmq_send (resposta, mediaMovel ,TAMANHO_STRING, 0);
+    		printf("%s",conteudoCliente);
     	}
-        else
-            printf("Requisicao nao encontrada!\n");
-        
+	else
+	       printf("Erro");
+
         sleep(10);
 
-    	fclose(arquivoCorrelacao);
-    	fclose(arquivoFibonacci);
-    	fclose(arquivoMediaMovel);
+	fclose(arquivoCorrelacao);
+	fclose(arquivoFibonacci);
+	fclose(arquivoMediaMovel);
     }
 
     return 0;
